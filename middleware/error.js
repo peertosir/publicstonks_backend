aa64ce39e421783c4bbbb2d9a7a5de0c7004cb33
Ilya Danilov <peertosir@gmail.com>
2020-04-29 01:44:31 +0300

    Added duplicate key error

--- middleware/error.js
+++ middleware/error.js
@@ -6,9 +6,16 @@ const errorHandler = (error, req, res, next) => {
         ...error
     };
     returnError.message = error.message;
+    console.log(error.red);
 
+    //bad objId
     if (error.name == 'CastError') {
         returnError = new ErrorResponse(`Resource with id ${error.value} not found`, 404);
+    }
+
+    //Duplicate key error
+    if (error.code === 11000) {
+        returnError = new ErrorResponse(`Duplicate field found`, 400);
     }
 
     res.status(returnError.statusCode || 500).json({
